97a27fd6cf020c02f583eb55bcf6d22509847cce
Dennis Eiben <dennis_eiben@web.de>
2025-04-24 22:27:46 +0200

    insert php umkommentieren

--- insert.php
+++ insert.php
@@ -1,20 +1,20 @@
 <?php
 require 'db.php';
-
+/*
 $dsn = "mysql:dbname=ticketsystem;host=localhost";
 $username = "root";
 $password = "";
 // PDO = php data objects - schnittstelle für die sql datenbank - dsn = data source dame = selbsterklaerend, suammengesestzt aus datenbank und host
 $con = new PDO($dsn, $username, $password);
+*/
 
 
 
-/*
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // Eingaben sichern gegen XSS
     $nickname = htmlspecialchars($_POST['nickname'], ENT_QUOTES, 'UTF-8');
     $email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
-    $entry = htmlspecialchars($_POST['password'], ENT_QUOTES, 'UTF-8');
+    $password = htmlspecialchars($_POST['password'], ENT_QUOTES, 'UTF-8');
 
     // Sicheres Einfügen mit PDO
     $stmt = $pdo->prepare("INSERT INTO entries (nickname, email, password) VALUES (:nickname, :email, :password)");
@@ -24,5 +24,5 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         echo "Fehler beim Speichern des Eintrags.";
     }
 }
-*/
+
 ?>
