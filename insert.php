11c2c6cd7b53e49a2866aa1ddd9e98866b76f619
Dennis Eiben <dennis_eiben@web.de>
2025-04-24 23:46:23 +0200

    tippfehlerregestrierung fehlerbehandlung

--- insert.php
+++ insert.php
@@ -16,13 +16,37 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
     $password = htmlspecialchars($_POST['password'], ENT_QUOTES, 'UTF-8');
 
-    // Sicheres Einfügen mit PDO
-    $stmt = $pdo->prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
-    if ($stmt->execute(['username' => $username, 'email' => $email, 'password' => $password])) {
-        echo "Account erfolgreich gespeichert!";
-    } else {
-        echo "Fehler beim erstellen des Accounts.";
+    //wenn das format der eingeben stimmt wird geprüft ob der nutzer bereits existiert
+    $stmt = $con->prepare("SELECT * FROM users WHERE username=:username OR email=:email"); //sucht in der db nach den eingegemenen user u. pw
+    // ersetzt die platzhalter mit den variablen
+    $stmt->bindParam(":username", $username);
+    $stmt->bindParam(":email", $email);
+    //führt das statement aus und holt sich einen user mit denselben variablen aus der db
+    $stmt->execute();
+    $userAlreadyExists = $stmt->fetchColumn();
+
+    if (!$userAlreadyExists)
+    {
+            // Sicheres Einfügen mit PDO
+            $stmt = $pdo->prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
+
+        if ($stmt->execute(['username' => $username, 'email' => $email, 'password' => $password]))
+        {
+            echo "Account erfolgreich gespeichert!";
+        }
+        else
+        {
+            echo "Fehler beim erstellen des Accounts.";
+        }
     }
+    else
+    {
+        echo "Account erfolgreich gespeichert!";
+    }
+}
+else
+{
+    echo "Eingeben ensprechen nicht den Erwartungen";
 }
 
 ?>
