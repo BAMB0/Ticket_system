a238e348a476c9c47badccd891b272c8f86ec16b
Dennis Eiben <dr.daswegen@gmail.com>
2025-04-25 09:53:29 +0200

    var.dump zum debuggen

--- post.php
+++ post.php
@@ -6,15 +6,18 @@ echo $_SESSION["username"];
 $username = $_SESSION["username"];  
 $text = htmlspecialchars($_POST['text'], ENT_QUOTES, 'UTF-8');  
 
-// Extract the id from the user table  
+// holt sich die User_id des eingeloggten Users
 $stmt = $pdo->prepare("SELECT id FROM users WHERE username = :username");  
 $stmt->bindParam(":username", $username, PDO::PARAM_STR); // <- der letzte teil ist wichtig, damit der username als string interpretiert wird
 $stmt->execute();  
 $user_id = $stmt->fetch(PDO::FETCH_ASSOC); //<- fetch gibt ein assoziatives Array zurück, das die User_id enthält
 
+// benutzt die user_id wird benutzt um die posts des nutzers zu differenzieren
 $stmt = $pdo->prepare("SELECT * FROM users WHERE user_id = :user_id");
 $stmt->bindParam(":post_id", $id, PDO::PARAM_INT);
 $stmt->bindParam(":post_content", $post_content, PDO::PARAM_STR);
-$stmt->bindParam(":post_date", $post_date, PDO::PARAM_DATE);
+$stmt->bindParam(":post_date", $post_date, PDO::PARAM_STR);
 $stmt->execute();
 $post = $stmt->fetch(PDO::FETCH_ASSOC);
+
+var_dump($post); // gibt den Inhalt des Posts aus
