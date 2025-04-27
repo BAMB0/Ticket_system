d8810118a0e281921c88cfd8eab8f71bc6b64ddf
BAMB0 <dr.daswegen@gmail.com>
2025-04-27 16:38:52 +0200

    test

--- insert.php
+++ insert.php
@@ -7,18 +7,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
     $password = PASSWORD_HASH($_POST['password'], PASSWORD_DEFAULT);
 
-    //wenn das format der eingeben stimmt wird geprüft ob der nutzer bereits existiert
+    //wenn das format der eingeben stimmt wird geprï¿½ft ob der nutzer bereits existiert
     $stmt = $pdo->prepare("SELECT * FROM users WHERE username=:username OR email=:email"); //sucht in der db nach den eingegemenen user u. pw
     // ersetzt die platzhalter mit den variablen
     $stmt->bindParam(":username", $username);
     $stmt->bindParam(":email", $email);
-    //führt das statement aus und holt sich einen user mit denselben variablen aus der db
+    //fï¿½hrt das statement aus und holt sich einen user mit denselben variablen aus der db
     $stmt->execute();
     $userAlreadyExists = $stmt->fetchColumn();
 
     if (!$userAlreadyExists)
     {
-            // Sicheres Einfügen mit PDO
+            // Sicheres Einfï¿½gen mit PDO
             $stmt = $pdo->prepare("INSERT INTO users (username, email, password) VALUES (:username, :email, :password)");
 
     if ($stmt->execute(['username' => $username, 'email' => $email, 'password' => $password]))
