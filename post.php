59b506b98912285709af60e6b27d23722520c5a9
BAMB0 <dr.daswegen@gmail.com>
2025-04-27 22:40:51 +0200

    +die uderid wird in die session geschrieben

--- post.php
+++ post.php
@@ -1,2 +1,13 @@
 <?php
+require 'db.php';
+session_start();
+echo $_SESSION["id"];  
+$user_id = $_SESSION["id"];
+var_dump($user_id); // gibt die User_id aus)
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $post_content = htmlspecialchars($_POST['post_content'], ENT_QUOTES, 'UTF-(');
+}
+
 ?>
